18c65b183fe3b6620574f64b29dcdc0a99c02a2f
Castro600 <castrohounmenou@gmail.com>
2025-05-25 15:24:24 +0200

    Ajout du fichier java script

---
+++ main.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/strict.dtd">
+<html>
+<head>
+<META http-equiv=Content-Type content="text/html; charset=UTF-8">
+<title>Exported from Notepad++</title>
+<style type="text/css">
+span {
+	font-family: 'Consolas';
+	font-size: 10pt;
+	color: #FFFFBF;
+}
+</style>
+</head>
+<script src='main.js'></script>
+<body>
+<div style="float: left; white-space: pre; line-height: 1; background: #000040; "></div></body>
+</html>
